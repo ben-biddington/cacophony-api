8862bcee5677adea4f9f48a08cc446c88615a2ce
ben.biddington <ben.biddington@gmail.com>
2018-06-11 10:40:14 +1200

    Delete node modules from container

--- docker-start.sh
+++ docker-start.sh
@@ -23,6 +23,7 @@ sudo docker build . -t cacophony-api
 sudo docker run -itd --rm --name cacophony-api-test -p 1080:1080 cacophony-api | tee > docker-container-pid
 
 sudo docker cp . cacophony-api-test:/
+sudo docker exec cacophony-api-test bash -c "$@ rm -r /node_modules"
 sudo docker exec cacophony-api-test bash -c "$@ echo 'npm install...'        && npm install &> echo '.'"
 sudo docker exec cacophony-api-test bash -c "$@ echo 'migrating database...' && node_modules/sequelize-cli/bin/sequelize db:migrate --config config/app.js"
 sudo docker exec cacophony-api-test bash -c "$@ node /Server.js"
