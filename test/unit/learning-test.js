2135af30286015a067392d3fd693060708c7c85b
ben.biddington <ben.biddington@gmail.com>
2018-06-11 15:26:13 +1200

    [?] Try and isolate `util.multipartDownload`

--- test/unit/learning-test.js
+++ test/unit/learning-test.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 
-# mocha test/unit/learning-test.js
+// mocha test/unit/learning-test.js
 describe('Awaiting promises', function() {
     it('the basics', async () => {
         const promise = new Promise((resolve, reject) => {
@@ -10,5 +10,29 @@ describe('Awaiting promises', function() {
         const result = await promise;
 
         assert.equal('a', result);
+    });
+});
+
+const config = require('../../config');
+
+console.log(config);
+
+//@todo: forced to do this as `Server.js` does.
+//@todo: path nonsense
+config.loadConfig('../../sauce/cacophony-api/config/app.js');
+
+var util = require("../../api/V1/util");
+
+// mocha test/unit/learning-test.js
+describe('file upload', function() {
+    it.only('the basics', async () => {
+        const fun = util.multipartDownload('file', {});
+
+        // impossible to know what args are supposed to look like
+        const result = await fun(
+            {
+                'part'
+            },
+            {});
     });
 });
