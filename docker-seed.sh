7c8d53d31d7f2f8c63cd73d8cf106704c4d06b5d
ben.biddington <ben.biddington@gmail.com>
2018-06-12 15:05:52 +1200

    Enable databas seeding

---
+++ docker-seed.sh
@@ -0,0 +1,2 @@
+INSERT INTO "Groups" (groupname) VALUES ('test-group');
+INSERT INTO "Users" (username, password, superuser) VALUES ('admin_test', 'admin_test', 1);
--- docker-start.sh
+++ docker-start.sh
@@ -1,10 +1,7 @@
 #/bin/bash
-#set -e
 
 if [ -f docker-container-pid ]; then
     pid="$(cat docker-container-pid)"
-
-    #echo $pid
 
     echo "Stopping container <$pid>"
     sudo docker stop $pid &> /dev/null
@@ -17,8 +14,6 @@ if [ -f docker-container-pid ]; then
     rm docker-container-pid
 fi
 
-#exit 1
-
 sudo docker build . -t cacophony-api
 sudo docker run -itd --rm --name cacophony-api-test -p 1080:1080 -p 9001:9001 cacophony-api | tee > docker-container-pid
 
@@ -26,9 +21,10 @@ sudo docker cp . cacophony-api-test:/
 sudo docker exec cacophony-api-test bash -c "$@ rm -r /node_modules"
 sudo docker exec cacophony-api-test bash -c "$@ echo 'npm install...'        && npm install 2> /dev/null"
 sudo docker exec cacophony-api-test bash -c "$@ echo 'migrating database...' && node_modules/sequelize-cli/bin/sequelize db:migrate --config config/app_test_default.js"
+sudo docker exec cacophony-api-test bash -c "sudo -i -u postgres psql cacophonytest -c \"\dt;\""
+sudo docker exec cacophony-api-test bash -c "sudo -i -u postgres psql cacophonytest -f docker-seed.sh"
 sudo docker exec cacophony-api-test bash -c "$@ MINIO_ACCESS_KEY=minio MINIO_SECRET_KEY=miniostorage ./minio server --address :9001 .data &> miniolog &"
 sudo docker exec cacophony-api-test bash -c "$@ sleep 10" # Allowing minio to initialize
 sudo docker exec cacophony-api-test bash -c "./mc config host add myminio http://127.0.0.1:9001 minio miniostorage"
 sudo docker exec cacophony-api-test bash -c "./mc mb myminio/cacophony"
-
 sudo docker exec cacophony-api-test bash -c "$@ node /Server.js --config=config/app_test_default.js"
