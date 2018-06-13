6fb92fb5d4588e4a5e9d14160e961a3f07089958
ben.biddington <ben.biddington@gmail.com>
2018-06-13 16:12:33 +1200

    Don't need to add group

--- test/db-seed.sql
+++ test/db-seed.sql
@@ -1,3 +1,2 @@
 -- sudo -i -u postgres psql {dbname} -f /test/db-seed.sql
-INSERT INTO "Groups" (groupname, "createdAt", "updatedAt") VALUES ('test-group', now(), now());
 INSERT INTO "Users" (username, password, superuser, "createdAt", "updatedAt") VALUES ('admin_test', '$2a$10$1//aJH6/g9duTPxFyqByZ.yHD0XYv2.d3748CkXR/1/V0mXLFTwM.', true, now(), now());
